c242a11fd744fd7831c986e3d396fedb21b4313a
AhmadWaleed <AhmadWaleed@users.noreply.github.com>
2021-03-09 06:24:50 +0000

    Fix styling

--- src/Soquel.php
+++ src/Soquel.php
@@ -2,16 +2,17 @@
 
 namespace AhmadWaleed\Soquel;
 
-use Omniphx\Forrest\Providers\Laravel\Facades\Forrest;
 use UnexpectedValueException;
+use Omniphx\Forrest\Providers\Laravel\Facades\Forrest;
 
 class Soquel
 {
     public static function authenticate(): array
     {
         $storage = ucwords(config('forrest.storage.type'));
-        if (! in_array($storage, [ 'Session', 'Cache' ]))
+        if (! in_array($storage, [ 'Session', 'Cache' ])) {
             throw new UnexpectedValueException("'$storage' is not a supported storage type for 'forrest.storage.type'. Please use 'Session' or 'Cache'.");
+        }
 
         if (! $storage::has(config('forrest.storage.path') . 'token')) {
             config()->set('forrest.authentication', 'UserPassword');
